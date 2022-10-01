d811bd2ae7c2258df64f5bc9079318ba743e25a5
Kazi Moazzem Hossain <19103143@iubat.edu>
2022-10-01 21:28:43 +0600

    General css & header section
    In this part I have defined the general css and defied the classes for the components. I have also finished styling the header section.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,25 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import { CTA } from "./CTA";
+import "./header.css";
+import ME from "../../asset/me.png";
+import { HeaderSocials } from "./HeaderSocials";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Moazzem Hossain</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
+  );
+};
